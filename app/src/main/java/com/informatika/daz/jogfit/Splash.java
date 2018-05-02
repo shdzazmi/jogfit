2bdc693c505e213d9c749fd3f7738e65abb71ab9
shdzazmi <shadazzazminuzband@gmail.com>
2018-05-02 18:02:27 +0800

    membuat tampilan Splash screen
    di splash screen ketika tekan tombol next akan menuju home screen

--- app/src/main/java/com/informatika/daz/jogfit/Splash.java
+++ app/src/main/java/com/informatika/daz/jogfit/Splash.java
@@ -1,13 +1,27 @@
 package com.informatika.daz.jogfit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Splash extends AppCompatActivity {
+
+    @BindView(R.id.imageButtonNext) ImageButton imageButtonNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.imageButtonNext)
+    public void next(){
+        Intent intent = new Intent(Splash.this, Home.class);
+        startActivity(intent);
     }
 }
